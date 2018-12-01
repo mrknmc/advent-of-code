bef6ce2935b45dbffdd6f7a559b30f622aff8f95
Mark Nemec <mrknmc@me.com>
2018-12-01 12:10:22 +0000

    File doesnt need to be mutable

--- src/bin/day01-part2.rs
+++ src/bin/day01-part2.rs
@@ -8,8 +8,8 @@ use std::collections::LinkedList;
 
 fn read_file(filename: String) -> LinkedList<i64> {
     let mut list = LinkedList::new();
-    let mut f = File::open(filename).expect("input file not found");
-    let mut buf_reader = BufReader::new(f);
+    let f = File::open(filename).expect("input file not found");
+    let buf_reader = BufReader::new(f);
     for line in buf_reader.lines() {
         list.push_back(line.unwrap().parse::<i64>().unwrap());
     }
@@ -20,6 +20,7 @@ fn main() {
     let filename = env::args().nth(1).expect("No argument filename passed");
     let list = read_file(filename);
     let mut seen = HashSet::new();
+    seen.insert(0);
     let mut sum = 0;
     for num in list.iter().cycle() {
         let res = sum + num;
