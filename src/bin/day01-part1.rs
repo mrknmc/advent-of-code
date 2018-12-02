0c9356ce833620f0769020596d14d7ee41c80f1a
Mark Nemec <mrknmc@me.com>
2018-12-02 12:58:47 +0000

    Fix warnings in day 1

--- src/bin/day01-part1.rs
+++ src/bin/day01-part1.rs
@@ -5,8 +5,8 @@ use std::io::BufRead;
 
 fn main() {
     let filename = env::args().nth(1).expect("No argument filename passed");
-    let mut f = File::open(filename).expect("input file not found");
-    let mut buf_reader = BufReader::new(f);
-    let total = buf_reader.lines().fold(0, |sum, l| l.unwrap().parse::<i64>().unwrap());
+    let f = File::open(filename).expect("input file not found");
+    let buf_reader = BufReader::new(f);
+    let total = buf_reader.lines().fold(0, |_sum, l| l.unwrap().parse::<i64>().unwrap());
     println!("{}", total);
 }
